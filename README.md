33c81520f759c06618d01062d9738eb54110b677
rwmpelstilzchen <foo@digitalwords.net>
2014-05-28 02:06:16 +0300

    Initial working release

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-freshest
-========
+=Freshest=
 
-A minimalist graphic customizable application menu with a simple-but-effective automatic reordering mechanism
+**Freshest** is a minimalist graphic customizable application menu with a simple-but-effective automatic reordering mechanism. It is written in Python with GTK+.
+
+How does it work? When you launch an application, it is moved to the head of the list. This way the most heavily used applications are concentrated in one end, and the least used ones in the other. This has two-way benefit, as most of the time you’ll want to use an application which you use frequently (duh!…), but sometimes you’d rather choose to launch one which gathers dust (e.g. that game you’ve downloaded months ago but haven’t got the chance to play and now you find it awesome).
+
+
+
+==Features==
+
+* Automatic **reordering** of items according to your changing habits and needs.
+* Everything is done in open human-readable, computer-manipulatable **standard formats** ([freedesktop.org](http://www.freedesktop.org/)’s [desktop entries](http://www.freedesktop.org/wiki/Specifications/desktop-entry-spec/) and [JSON](http://www.json.org/)). Desktop entries can be copied (or symlinked) from your system’s appropriate directory (e.g. `/usr/share/applications` in [http://archlinux.org](Arch Linux)).
+* Native support for **localization** ([POSIX locale](https://en.wikipedia.org/wiki/Locale#POSIX_platforms)). If a desktop entry file doesn’t contain a name suitable for your language, you can [add it manually](http://standards.freedesktop.org/desktop-entry-spec/latest/ar01s04.html).
+* While manipulating the menu requires some technical skills, using the menu is dead **simple**, making it perfect for children (my kids use it for launching games).
+
+
+
+===Usage===
+
+One can have several independent profiles; they are located by default each at `~/.config/freshest/PROFILENAME`, where the default `PROFILENAME` is `default`. The latter is used by running `freshest`, while other profiles can be used by running `freshest PROFILENAME` (this will create a new profile in case it doesn't exist yet).
+
+Each profile has:
+* `applications/`: a subdirectory which you should populate with `.desktop` files
+* `list.json`: an automatically managed file which contains data about which applications was used most recently
+* `config.json`: a configuration file
+
+Desktop entry files can be easily edited with any text editor. If you want a specialized tool, give [this](https://github.com/MicahCarrick/desktop-entry-editor) a try.
+
+
+
+===Contribution==
+
+Feel free to contribute to this software ☺. Just fork it, make the changes you want and send a pull request.
+
+I’m just a novice Python programmer. I’m aware the code isn’t elegant; if you have suggestions for making it more readable, efficient, neat or Pythonic, don’t hesitate.
+
+
+
+==Dependencies==
+
+* [PyGObject](https://live.gnome.org/PyGObject) 3, for GUI (Arch `pygobject-devel`; Fedora `pygobject3`; Ubuntu `python-gobject`)
+* [PyXDG](http://freedesktop.org/wiki/Software/pyxdg/), for parsing `.desktop` files (Arch and Ubuntu `python-xdg`; Fedora `pyxdg`)
+
+
+
+==License==
+
+[Abolish intellectual property](https://en.wikipedia.org/wiki/Anti-copyright_movement)! ☠
---
+++ freshest.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+import glob
+from gi.repository.GdkPixbuf import Pixbuf
+from gi.repository import Gtk, GdkPixbuf
+import os
+import json
+import subprocess
+import sys
+from xdg import DesktopEntry
+
+
+class Freshest:
+    def __init__(self, profile="default"):
+        try:
+            confdir = os.environ["XDG_CONFIG_HOME"]
+        except KeyError:
+            confdir = os.path.join(os.environ["HOME"], ".config")
+        self.profiledir = os.path.join(confdir, "freshest", profile)
+        self.appsdir = os.path.join(self.profiledir, "applications/")
+        self.listfile = os.path.join(self.profiledir, "list.json")
+        self.conffile = os.path.join(self.profiledir, "config.json")
+
+        if not os.path.isdir(self.profiledir):
+            self.create_profile(self.profiledir)
+
+        self.__desktop_files = self.update_list()
+
+        f = open(self.conffile, "r")
+        config = json.load(f)
+        f.close()
+
+        wintitle = "Freshest"
+        if profile != "default":
+            wintitle += " (" + profile + ")"
+        self.__window = Gtk.Window(title=wintitle)
+        self.__window.set_default_size(config["width"], config["height"])
+        self.__window.move(config["x"], config["y"])
+
+        scrolledwindow = Gtk.ScrolledWindow()
+
+        liststore = Gtk.ListStore(Pixbuf, str)
+        iconview = Gtk.IconView.new()
+        iconview.set_model(liststore)
+        iconview.set_pixbuf_column(0)
+        iconview.set_text_column(1)
+        iconview.set_item_width(config["columnwidth"])
+
+        for f in self.__desktop_files:
+            x = DesktopEntry.DesktopEntry(filename=f)
+            try:
+                pixbuf = Gtk.IconTheme.get_default().load_icon(
+                    x.getIcon(), config["iconsize"], 0)
+            except:
+                try:
+                    pixbuf = Pixbuf.new_from_file(x.getIcon())
+                    pixbuf = pixbuf.scale_simple(
+                        config["iconsize"], config["iconsize"],
+                        GdkPixbuf.InterpType.BILINEAR)
+                except:
+                    pixbuf = Gtk.IconTheme.get_default().load_icon(
+                        "gtk-execute", config["iconsize"], 0)
+            liststore.append([pixbuf, x.getName()])
+
+        iconview.connect("selection_changed", self.on_icon_view_selection_changed)
+
+        scrolledwindow.add(iconview)
+
+        self.__window.connect("destroy", lambda w: Gtk.main_quit())
+
+        self.__window.add(scrolledwindow)
+        self.__window.show_all()
+
+    def create_profile(self, profile):
+        os.makedirs(self.profiledir)
+        os.mkdir(self.appsdir)
+        f = open(self.listfile, "w")
+        json.dump([], f)
+        f.close()
+        f = open(os.path.join(self.appsdir, "example.desktop"), "w")
+        f.write("""[Desktop Entry]
+Type=Application
+Name=Add *.desktop files to """ + self.appsdir + """
+Icon=gtk-info
+Exec=true""")
+        f.close()
+        f = open(self.conffile, "w")
+        config = {"x": 100, "y": 100,
+                  "width": 640, "height": 480,
+                  "columnwidth": 96, "iconsize": 96}
+        json.dump(config, f, sort_keys=True, indent=4 * ' ')
+        f.close()
+
+    def update_list(self):
+        # read from the list file, which is ordered but not necessarily
+        # up-to-date
+        f = open(self.listfile, 'r')
+        old = json.load(f)
+        f.close()
+
+        # read actual files
+        new = []
+        actual_files = glob.glob(os.path.join(self.appsdir,
+                                              "*.desktop"))
+        for actual_file in actual_files:
+            new.append(actual_file)
+
+        # merge
+        for olditem in old:
+            if olditem not in new:
+                old.remove(olditem)
+        for newitem in new:
+            if newitem not in old:
+                old.append(newitem)
+
+        # update file
+        f = open(self.listfile, 'w')
+        json.dump(old, f, indent=4 * ' ')
+        f.close()
+
+        return old
+
+    def update_freshest(self, application):
+        self.__desktop_files.remove(application)
+        self.__desktop_files.insert(0, application)
+        f = open(self.listfile, 'w')
+        json.dump(self.__desktop_files, f, indent=4 * ' ')
+        f.close()
+
+    def on_icon_view_selection_changed(self, widget):
+        self.on_item_activated(widget, widget.get_selected_items()[0])
+
+    def on_item_activated(self, widget, item):
+        selected_application = self.__desktop_files[int(item.to_string())]
+        self.update_freshest(selected_application)
+
+        # save position and size of the window
+        f = open(self.conffile, "r")
+        config = json.load(f)
+        f.close()
+        f = open(self.conffile, "w")
+        winpos = self.__window.get_position()
+        winsize = self.__window.get_size()
+        config["x"] = winpos[0]
+        config["y"] = winpos[1]
+        config["width"] = winsize[0]
+        config["height"] = winsize[1]
+        json.dump(config, f, sort_keys=True, indent=4 * ' ')
+        f.close()
+
+        x = DesktopEntry.DesktopEntry(filename=selected_application)
+        subprocess.Popen(x.getExec(), shell=True)
+        quit()
+
+if len(sys.argv) == 2:
+    Freshest(profile=sys.argv[1])
+else:
+    Freshest()
+Gtk.main()
